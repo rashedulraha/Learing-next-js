f2de5345f184cd8316dd2514eb9502e56832df28
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-05 15:34:53 +0600

    rouding metadata

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import type { Metadata } from "next";
 
-const Login = () => {
+type Props = {
+  params: {
+    productsId: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  return {
+    title: `Login Page ${params.productsId}`,
+    description: `Please Login ${params.productsId}`,
+  };
+};
+
+export default function Login() {
   return (
     <div>
       <h1>Login</h1>
     </div>
   );
-};
-
-export default Login;
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "About description",
+};
 
 const page = () => {
   return <div>Default function about</div>;
