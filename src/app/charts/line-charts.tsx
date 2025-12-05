c46cb47959467c709044a6ce9d68884f2daab400
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-05 12:07:01 +0600

    File Colocation

---
+++ src/app/charts/line-charts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LineCharts = () => {
+  return (
+    <div>
+      <h1>Line Charts</h1>
+    </div>
+  );
+};
+
+export default LineCharts;
---
+++ src/app/charts/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LineChart = () => {
+  return <h2>Hello i&apos;m not waking</h2>;
+};
+
+const LineCharts = () => {
+  return (
+    <div>
+      <h1>Line Charts Upgrade</h1>
+      {LineChart()}
+    </div>
+  );
+};
+
+export default LineCharts;
