714518eea243a98bd1010a4df71e1b90b1b33e24
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-13 14:47:43 +0600

    ceate new project

--- src/Components/Responsive/Container.tsx
+++
@@ -1,7 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-export default function Container({ children }: ContainerProps) {
-  return <div className="container mx-auto px-4">{children}</div>;
-}
