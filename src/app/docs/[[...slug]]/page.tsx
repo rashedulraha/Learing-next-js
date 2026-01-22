f67f6f5817bacc8746fbeab1e1a54a7e76aa8231
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-22 14:24:51 +0600

    dynamic routing

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DocsPage = () => {
+  return (
+    <div>
+      <h1>Hello docs page</h1>
+    </div>
+  );
+};
+
+export default DocsPage;
---
+++ src/app/shop/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Hello shops</h1>
+    </div>
+  );
+};
+
+export default Shop;
