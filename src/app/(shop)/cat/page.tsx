0a260fc615922696e70087be54914e64692cee78
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-22 14:30:48 +0600

    non routeble

---
+++ src/app/(shop)/cat/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CatPage = () => {
+  return (
+    <div>
+      <h2> Hello cat page</h2>
+    </div>
+  );
+};
+
+export default CatPage;
---
+++ src/app/non-routable/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Example from "./_components/example";
+
+const NonRoutable = () => {
+  return (
+    <div>
+      <h2>Non Routable</h2> and <Example />
+    </div>
+  );
+};
+
+export default NonRoutable;
