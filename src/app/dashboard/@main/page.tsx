464079baec3c0367999b79d5febd2d540236e901
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-22 16:51:28 +0600

    intercept and other

--- src/app/dashboard/@main/page.tsx
+++ src/app/dashboard/@main/page.tsx
@@ -5,9 +5,9 @@ export default function MainContent() {
     <div>
       <h2>User List</h2>
 
-      <Link href="/dashboard/users/1">User 1</Link>
+      <Link href="/dashboard/users">User 1</Link>
       <br />
-      <Link href="/dashboard/users/2">User 2</Link>
+      <Link href="/dashboard/users">User 2</Link>
     </div>
   );
 }
