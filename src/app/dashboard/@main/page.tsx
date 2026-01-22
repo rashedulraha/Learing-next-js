2de8037d3652843394988f9abadd742c9bc8a959
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-22 16:24:30 +0600

    inter ceptor

--- src/app/dashboard/@main/page.tsx
+++ src/app/dashboard/@main/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function MainContent() {
-  return <h1>Dashboard Content</h1>;
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <Link href="/dashboard/users/1">User 1</Link>
+      <br />
+      <Link href="/dashboard/users/2">User 2</Link>
+    </div>
+  );
 }
