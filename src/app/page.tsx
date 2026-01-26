8b0604edd826087b646f0538a978d9a21e4d530a
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-26 17:55:37 +0600

    adding SparklesPreview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { SpotlightPreview } from "@/components/SpotlightPreview";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <h1 className="font-bold text-4xl">Learning next js </h1>
+        <SpotlightPreview />
       </main>
     </div>
   );
