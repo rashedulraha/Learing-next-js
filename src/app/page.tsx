85e17869352a8f9f41f8c1b2d9d74e58c8672ca9
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-26 17:48:18 +0600

    adding shadcn and other aceternity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <main>
-      <h1 className="font-bold text-4xl">Learning next js </h1>
-     </main>
+      <main>
+        <h1 className="font-bold text-4xl">Learning next js </h1>
+      </main>
     </div>
   );
 }
