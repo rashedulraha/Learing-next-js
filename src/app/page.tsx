47dc4bbb913cfe2a8a680413b63abb3d055a6d71
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-26 18:07:24 +0600

    BackgroundBoxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { SpotlightPreview } from "@/components/SpotlightPreview";
+// import { SparklesPreview } from "@/components/SparklesPreview";
+import { BackgroundBoxesDemo } from "@/components/BackgroundBoxesDemo";
+// import { SpotlightPreview } from "@/components/SpotlightPreview";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <SpotlightPreview />
+        {/* <SpotlightPreview /> */}
+        {/*adding more ui using shadcn and aceternity */}
+        {/* <SparklesPreview /> */}
+        {/*  more bg  */}
+        <BackgroundBoxesDemo />
       </main>
     </div>
   );
