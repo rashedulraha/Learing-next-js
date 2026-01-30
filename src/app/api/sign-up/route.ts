f9b29cbdbf98bdf35376692c24502e8e3aea29a1
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-30 10:01:06 +0600

    update package  and simple.env and other signup route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,10 +1,35 @@
 import dbConnect from "@/lib/dbConnection";
+import UserModel from "@/model/User";
+import bcrypt from 'bcrypt';
 
 export async function POST(request: Request) {
   await dbConnect();
 
   try {
     const { username, email, password } = await request.json();
+    // existing  user verification  by user name
+    const existingUserVerifiedByUserName = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    //
+
+    if (existingUserVerifiedByUserName) {
+      return Response.json(
+        { success: false, message: "User already taken" },
+        { status: 400 },
+      );
+    }
+
+    //  existing user by email
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    if (existingUserByEmail) {
+      // true  TODO back here
+    } else {
+      const  hashPassWord = 
+    }
   } catch (signUpError) {
     console.log("Error registering user :", signUpError);
     return Response.json(
